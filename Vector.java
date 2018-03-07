ea5be7baaae2ebc3554216c56702a1fa63b3cb7a
Aaron Smith <33443869+aaronsjsu@users.noreply.github.com>
2018-03-06 18:23:28 -0800

    Add files via upload
    Initial commit

---
+++ Vector.java
@@ -0,0 +1,18 @@
+package linear_algebra;
+
+/**
+ * This class models a vector (both column and row vectors) by 
+ * using an array of doubles to represent vector values. The vector
+ * can then be used to solve problems in Linear Algebra.
+ * @author Aaron Smith
+ */
+public class Vector {
+	
+	private double[] vector;
+	
+	public Vector(double[] vector) {
+		this.vector = vector;
+	}
+	
+	
+}
